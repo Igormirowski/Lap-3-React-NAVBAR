8cdc50e3b26bc58ec20a77e6bfa996bbadc2d924
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-20 11:16:17 +0100

    link to main page with NotFound

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -5,9 +5,14 @@ import { useLocation, Link } from 'react-router-dom';
 const NotFound = () => {
     const location = useLocation()
     return(
+        <>
         <h1>
        Sorry, {location.pathname} page not found!
+       <Link to='/'>
+       go back to main page
+       </Link>
         </h1>
+        </>
     )
 
 }
